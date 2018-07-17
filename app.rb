67745af99d9cf76df7631de884aff97b92cb3fb4
sophiahusak <72futbolplayer@gmail.com>
2018-07-17 18:29:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 class App < Sinatra::Base
-
-  # Define your GET '/' route below and respond with "Hello, World!"
+  
+class App
+  get '/' do
+    "Hello World"
+  end
+end # Define your GET '/' route below and respond with "Hello, World!"
 
 end
